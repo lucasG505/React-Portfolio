ac19f6699da9f46b92a219a161678fa317c214a6
Lucas Gonzalez <lucas.mariano.g@gmail.com>
2024-02-01 17:31:03 -0300

    Change of picture

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -11,7 +11,7 @@ export const Intro=()=>{
             <p className={styles.descrip}>I´m a fullStack developer starting his career using React, Express and Node.js. I´m in constant capacitation and looking to improve my skills, if you´re interested in working together reach out to me!</p>
             <a className={styles.contactBtn} href="mailto:lucas.mariano.g@gmail.com">Contact me</a>
         </div>
-        <img className={styles.heroImg} src={getImageUrl("intro/heroCircular.png")} alt="hero" />
+        <img className={styles.heroImg} src={getImageUrl("intro/me.png")} alt="hero" />
         <div className={styles.topBlur}/>
         <div className={styles.botBlur}/>
     </section>;
